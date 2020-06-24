41183c9f442042bb3acb4e04f259a821f96ff282
nysyo <tds24004@gmail.com>
2020-06-24 16:23:46 +0900

    e->elm var\>const

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function toAC(){
-    let e = document.getElementsByTagName('img')
+    let elm = document.getElementsByTagName('img')
     const completeByManual = 'https://wsdmoodle.waseda.jp/theme/image.php/boost/core/1592591198/i/completion-manual-y'
     const completeByAuto = 'https://wsdmoodle.waseda.jp/theme/image.php/boost/core/1592591198/i/completion-auto-y'
-    for (let i = 0; i < e.length; i++){
-        if(e[i].hasAttribute('src') && (e[i].getAttribute('src') == completeByManual || e[i].getAttribute('src') == completeByAuto)){
-            e[i].setAttribute('src','https://i.imgur.com/iwVr5TQ.png')
+    for (let i = 0; i < elm.length; i++){
+        if(elm[i].hasAttribute('src')){
+            if(elm[i].getAttribute('src') == completeByManual || elm[i].getAttribute('src') == completeByAuto){
+                elm[i].setAttribute('src','https://i.imgur.com/iwVr5TQ.png')
+            }
         }
     }
 }
-var observer = new MutationObserver(toAC);
+const observer = new MutationObserver(toAC);
 observer.observe(document.head, {
     attributes: true,
     childList:  true
 });
-toAC();
+toAC()
